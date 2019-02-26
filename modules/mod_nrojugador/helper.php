768aa635ac69f418e9afbce78068cabcb9c83093
Federico "Magus" Tomas Rocca <frocca17@gmail.com>
2019-02-25 23:52:16 -0300

    Ahora el modulo si encuentra en numero de jugador valido lo carga y lo muestra, caso contrario, muestra una imagen que linkea al formulario de registro.

--- modules/mod_nrojugador/helper.php
+++ modules/mod_nrojugador/helper.php
@@ -1,62 +1,29 @@
 <?php
-/**
- * Helper class for Hello World! module
- * 
- * @package    Joomla.Tutorials
- * @subpackage Modules
- * @link http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
- * @license        GNU/GPL, see LICENSE.php
- * mod_helloworld is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
- */
 class nroJugador
 {
-    /**
-     * Retrieves the hello message
-     *
-     * @param   array  $params An object containing the module parameters
-     *
-     * @access public
-     */    
     public static function getplayerNumberByUserID($params)
     {
         $db = JFactory::getDbo();
-        // $query = $db->getQuery(true);
-        // $query->select($db->quoteName(array('user_id', 'profile_key', 'profile_value', 'ordering')));
-        // $query->from($db->quoteName('#__user_profiles'));
-        // $query->where($db->quoteName('profile_key') . ' LIKE '. $db->quote('\'custom.%\''));
-        // $query->order('ordering ASC');
-        //$sql = "SELECT * FROM `jos_comprofiler`";
-        
         $database =& JFactory::getDBO();
-        
         $user = JFactory::getUser();
 
         $query = "SELECT cb_nrodejugador FROM `jos_comprofiler` where user_id = " .$user->id. ";";
         $database->setQuery($query);
-        //$result = $database->query();
         $result = $database->loadResult();
 
         // chequeo si $database->loadResult(); volvió vacia, si es así, no tiene cargado el número de jugador
 	if(isset($result) == false || $result == 0)
 	{
-		//Variable no seteada
-		echo "----------------";
-		echo "Variable no seteada";
-		echo "----------------";
-
+		//El Nro jugador no esta dado de alta
+                $doc = new DOMDocument();
+                $doc->loadHTML('<a href="https://docs.google.com/forms/d/e/1FAIpQLSc0Wyy7adG05u67cJnHxyMqMQ5GxZUWJHw2q7elRul3ick-qA/viewform" target="_blank" rel="noopener noreferrer" title="Registrate!"><img src="/images/boton2.png" alt=""></a>');
+                return $doc->saveHTML();
 	}
 	else
 	{
-		//La variable está seteada
-		echo "----------------";
-                echo "Variable seteada";
-                echo "----------------";
-
+                //El Nro jugador esta dado de alta
+                $result = 'Tu número de jugador es el ' . $result;
+                return $result;
 	}
-
-        return $result;
     }
 }
--- modules/mod_nrojugador/tmpl/default.php
+++ modules/mod_nrojugador/tmpl/default.php
@@ -1,4 +1,4 @@
 <?php 
 // No direct access
 defined('_JEXEC') or die; ?>
-<?php echo 'Tu número de jugador es '.$PlayerNumber; ?>
+<?php echo $PlayerNumber; ?>
